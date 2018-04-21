0031497e6c7a05ddedae106c9583b344e49cbe9b
jukka <chryselectrum@gmail.com>
2018-04-21 18:36:27 +0300

    clean

--- currencies/xrb.js
+++ currencies/xrb.js
@@ -23,15 +23,13 @@ XRB = class XRB {
   start(txCb, blockCb) {    
 	
 	var sockets = [null, null];		
-	var baseLength = this.baseUrls.length;
-	//console.log('url length: ' + baseLength);
-	//console.log(sockets);
+	var baseLength = this.baseUrls.length;		
 	for (var i = 0; i < baseLength; i++)	
 	{	
 		//console.log('i: ' + i + ', baseUrl: ' + this.baseUrls[i]);		
 		if (i >= baseLength) 
 			return;	
-		var socket = io(this.baseUrls[i],{ forceNew: true });
+		var socket = io(this.baseUrls[i]); // ,{ forceNew: true }
 		
 		console.log(socket);
 		socket.on('connect', function () {
@@ -56,73 +54,13 @@ XRB = class XRB {
 		sockets[i] = socket;
 	}
 	//console.log(sockets);		
-	
 		
-	/*	
-	if (this.ws) {
-      this.stop();
-    }
-	
-    this.ws = new WebSocket(this.socketUrl);
-    this.ws.onclose = () => {
-      return setTimeout((() => {
-        return this.start(txCb, blockCb);
-      }), 1000);
-    };
-    this.ws.onopen = () => {
-      this.ws.send('2probe');
-      this.ws.send('5');
-      this.ws.send('420["subscribe","sync"]');
-      this.ws.send('421["subscribe","inv"]');
-      this.ws.send('422["subscribe","sync"]');
-      this.ws.send('424["subscribe","sync"]');
-      this.ws.send('425["subscribe","inv"]');
-      return this.ping = setInterval((() => {
-        return this.ws.send('2');
-      }), 25 * 1000);
-    };
-    return this.ws.onmessage = ({data}) => {	  
-	  //console.log('transaction data:')		
-	  console.log({data})
-      var payload, type;
-      data = data.match(/^\d+(\[.+?)$/);
-      if (data) {
-        [type, payload] = JSON.parse(data[1]);
-        if (type === 'tx') {
-          // fetch fees every now and then
-          if (new Date().getTime() - this.txFeeInterval > this.txFeeTimestamp) {
-            $.get(this.txApi + payload.txid, ({fees}) => {
-              if (fees) {
-                this.txFees.shift();
-                this.txFees.push(fees);
-                return this.txFeeTimestamp = new Date().getTime();
-              }
-            });
-          }
-          return typeof txCb === "function" ? txCb({
-            amount: payload.valueOut,
-            fee: Math.random() * Math.abs(this.txFees[0] - this.txFees[1]) + Math.min.apply(0, this.txFees),
-            link: this.baseURL + 'tx/' + payload.txid,
-            donation: !!payload.vout.find((vout) => {
-              return Object.keys(vout)[0] === this.donationAddress;
-            })
-          }) : void 0;
-        } else {
-          return $.get(this.blockApi + payload, ({tx}) => {
-            return typeof blockCb === "function" ? blockCb({
-              count: tx ? tx.length : 0
-            }) : void 0;
-          });
-        }
-      }
-    };
-	*/
   }
 
   stop() {
-    this.ws.close();
-    clearInterval(this.ping);
-    return this.ws = null;
+	  
+    for (var i = 0; i < this.baseUrls.length; i++)
+		if (socket[i]) sockets[i].emit('end');		
   }
 
 };
